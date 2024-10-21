f208885a347cebb16d10a05addd6bfe44b47d070
RakDuhl <mikael@hake.ch>
2024-10-21 22:53:55 +0200

    element and material registry insanity...

--- kubejs/startup_scripts/GTAtomphysx/elements.js
+++ kubejs/startup_scripts/GTAtomphysx/elements.js
@@ -10,16 +10,25 @@
 elementRegistry(
     event => {
         //Arcane Components
-        event.create('mana',            1, 1, -1, null, '✬', false) 
-        event.create('mana_steel',      27, 33, -1, null, 'Fe₃✬₃', false) //Most stable Fe isotope used, 56Fe, 26 Protons, 32 Neutrons
-        event.create('terra_steel',     42, 51, -1, null, 'Fe₃✬₆V⁽³⁾₃', false)
-        event.create('mana_pearl',      -1, -1, -1, null, '✬₆BeK₄N₅', false)
-        event.create('arcane_gold',     79, 118, -1, null, 'Au₃✬₃', false) //naturally occuring Gold isotope used, ¹⁹⁷Au
-        event.create('alfsteel',        58, 72, -1, null, 'Fe₃✬₆V⁽³⁾₃', false)
+        event.create('Mana',            1, 1, -1, null, '✬', false)
+        event.create('Creativity',      -1, -1, -1, null, 'Create', false)
+//        event.create('Mana Steel',      27, 33, -1, null, 'Fe₃✬₃', false) //Most stable Fe isotope used, 56Fe, 26 Protons, 32 Neutrons
+//        event.create('Terra Steel',     42, 51, -1, null, 'Fe₃✬₆V⁽³⁾₃', false)
+//        event.create('Mana Pearl',      -1, -1, -1, null, '✬₆BeK₄N₅', false)
+//        event.create('Arcane Gold',     79, 118, -1, null, 'Au₃✬₃', false) //naturally occuring Gold isotope used, ¹⁹⁷Au
+//        event.create('Alfsteel',        58, 72, -1, null, 'Fe₃✬₆V⁽³⁾₃', false)
 
         //Custom Elements
-        event.create('adamantium',      58, 82, -1, null, 'Ad', false)
-        event.create('mithril',         18, 22, -1, null, 'My', false)
-        event.create('hihiirokane',     29, 34, -1, null, 'Hk', false)
+        event.create('Adamantium',      58, 82, -1, null, 'Ad', false)
+        event.create('Vulcanite', 1, 1, -1, null, 'Vc', false)
+        event.create('Hihiirokane',     29, 34, -1, null, 'Hk', false)
+        event.create('Prometheum', 1, 1, -1, null, 'Ph', false)
+        event.create('Orichalcum', 1, 1, -1, null, 'Oc', false)
+//        event.create('Mithril',         18, 22, -1, null, 'Ag✬', false)
+//        event.create('Dawnstone', 1, 1, -1, null, 'Dw', false)
+//        event.create('Duskstone', 1, 1, -1, null, 'Dk', false)
+//        event.create('Astral Silver', 1, 1, -1, null, '', false)
+//        event.create('', 1, 1, -1, null, '', false)
+//        event.create('', 1, 1, -1, null, '', false)
     }
 )
--- kubejs/startup_scripts/GTAtomphysx/materials.js
+++ kubejs/startup_scripts/GTAtomphysx/materials.js
@@ -1,169 +1,262 @@
 // priority: 8900
 
-GTCEuStartupEvents.materialModification(
-    event => {
-        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('mana_steel'),   () => Item.getItem('botania:manasteel_block'))
-        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('mana_steel'),   () => Item.getItem('botania:manasteel_ingot'))
-        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('mana_steel'),   () => Item.getItem('botania:manasteel_nugget'))
-        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('terra_steel'),  () => Item.getItem('botania:terrasteel_block'))
-        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('terra_steel'),  () => Item.getItem('botania:terrasteel_ingot'))
-        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('terra_steel'),  () => Item.getItem('botania:terrasteel_nugget'))
-        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcane_gold'),  () => Item.getItem('wizards_reborn:arcane_gold_block'))
-        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcane_gold'),  () => Item.getItem('wizards_reborn:arcane_gold_ingot'))
-        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcane_gold'),  () => Item.getItem('wizards_reborn:arcane_gold_nugget'))
-        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('alfsteel'),     () => Item.getItem('mythicbotany:alfsteel_block'))
-        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('alfsteel'),     () => Item.getItem('mythicbotany:alfsteel_ingot'))
-        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('alfsteel'),     () => Item.getItem('mythicbotany:alfsteel_nugget'))
-    }
-);
+//Superscript   ¹²³⁴⁵⁶⁷⁸⁹⁰⁺⁻⁼⁽⁾
+//Subscript     ₁₂₃₄₅₆₇₈₉₀₊₋₌₍₎
+//Special characters ✬
+//Ag₄✬₆
 
 GTCEuStartupEvents.registry(
     'gtceu:material',
     event => {
-        event.create('mana_steel')
-        //Fe₃✬₃
-            .composition('3x iron', '3x mana')
-            .fluid()
-            .fluidTemp(823)
-            .color(0x67b9ee)
-            .secondaryColor(0xB5FFFC)
-            .element(GTElements.get('mana_steel'))
-            .cableProperties(V('lv'), 2, 0, true)
-            .iconSet(METALLIC)
-            .flags(
-                foil,
-                fine_wire,
-                lens,
-                plates,
-                rod,
-                frame,
-                gear,
-                long_rod,
-                no_abs_recipe
-            );
 
-        event.create('terra_steel')
-        //Fe₃✬₆V⁽³⁾₃
-            .composition('3x iron', '6x mana', '3x vanadium')
-            .fluid()
-            .fluidTemp(1682)
-            .color(0x55f609)
-            .element(GTElements.get('terrasteel'))
-            .iconSet(SHINY)
-            .cableProperties(V('ev'), 4, 0, true)
-            .flags(
-                foil,
-                fine_wire,
-                lens,
-                plates,
-                rod,
-                frame,
-                gear,
-                long_rod,
-                no_abs_recipe 
-            );
-        
-
-        event.create('alfsteel')
-            .color(0xFFC74E)
-            .fluid()
-            .fluidTemp(2045)
-            .secondaryColor(0xE27500)
-            .element(GTElements.get('alfsteel'))
-            .iconSet(SHINY)
-            .cableProperties(V('luv'), 6, 0, true)
-            .flags(
-                foil,
-                fine_wire,
-                lens,
-                plates,
-                rod,
-                frame,
-                gear,
-                long_rod,
-                no_abs_recipe 
-            );
+        event.create('mana')
+//        .components('2x mana')
+        .element(GTElements.get('Mana'))
+        .fluid()
+        .color(0x20DAFF);
         
         event.create('adamantium')
-            .ingot()
-            .fluid()
-            .fluidTemp(4210)
-            .element(GTElements.get('adamantium'))
-            .color(0xAD0F0D)
-            .secondaryColor(0x64082B)
-            .iconSet(DULL)
-            .fluid()
-            .cableProperties(V('zpm'), 8, 0, true)
-            .flags(
-                no_unify,
-                plates,
-                dense_plate,
-                rod,
-                bolt_and_screw,
-                frame,
-                gear,
-                long_rod,
-                block,
-                foil,
-                ring,
-                fine_wire,
-                no_smelt,
-                no_abs_recipe
-            )
-            .blastTemp(4210, 'mid', VA('luv'), 32)
-            .ore();
+        .element(GTElements.get('Adamantium'))
+        .color(0xAD0F0D).secondaryColor(0x64082B).iconSet(DULL)
+        .ingot()
+        .fluid()
+        .ore()
+        .cableProperties(V('zpm'), 8, 0, false)
+        .blastTemp(4210, 'high', 24800, 320)
+        .flags(
+            no_unify,
+            plates,
+            dense_plate,
+            rod,
+            bolt_and_screw,
+            frame,
+            gear,
+            long_rod,
+            block,
+            foil,
+            ring,
+            fine_wire,
+            no_smelt,
+            no_abs_recipe
+        );
         
         event.create('mithril')
-            .ingot()
-            .fluid()
-            .fluidTemp(750)
-            .element(GTElements.get('mithril'))
-            .color(0xd6edff)
-            .secondaryColor(0x9BFFFF)
-            .iconSet(SHINY)
-            .fluid()
-            .cableProperties(V('ev'), 2, 0, true)
-            .flags(
-                plates,
-                dense_plate,
-                rod,
-                frame,
-                gear,
-                small_gear,
-                long_rod,
-                block,
-                foil,
-                fine_wire,
-                no_abs_recipe
-            )
-            .blastTemp(750, null, VA('mv'), 300)
-            .ore();
+        //Ag₄✬₆Ti
+        .components('4x silver', '6x mana', 'titanium')
+        .color(0xd6edff).secondaryColor(0x9BFFFF).iconSet(SHINY)
+        .ingot()
+        .fluid()
+        .ore()
+        .cableProperties(V('ev'), 2, 0, false)
+        .blastTemp(750, 'mid', 288, 300)
+        .flags(
+            plates,
+            dense_plate,
+            rod,
+            frame,
+            gear,
+            small_gear,
+            long_rod,
+            block,
+            foil,
+            fine_wire,
+            no_abs_recipe
+        );
         
         event.create('arcane_gold')
         //Au₃✬₃
-            .element(GTElements.get('arcane_gold'))
-            .color(0xEDC992)
-            .iconSet(METALLIC)
-            .fluid()
-            .cableProperties(V('ev'), 3, 0, true)
-            .flags(
-                foil,
-                fine_wire,
-                lens,
-                plates,
-                rod,
-                frame,
-                gear,
-                long_rod,
-                solder_mat_good,
-                no_abs_recipe
-            );
+        .components('3x gold', '3x mana')
+        .color(0xEDC992).iconSet(SHINY)
+        .fluid()
+        .ingot()
+        .cableProperties(V('ev'), 3, 0, true)
+        .flags(
+            foil,
+            fine_wire,
+            plates,
+            rod,
+            frame,
+            gear,
+            long_rod,
+            solder_mat_good
+        );
+        
+        event.create('vulcanite')
+        //
+        .element(GTElements.get('Vulcanite'))
+        .color(0xFFB08A).secondaryColor(0xF24F00).iconSet(DULL)
+        .ingot()
+        .fluid()
+        .ore()
+        .cableProperties(V('iv'), 2, 1, false)
+        .blastTemp(2320, 'mid', 3280, 250)
+        .flags(
+            plates,
+            dense_plate,
+            rod,
+            gear,
+            small_gear,
+            long_rod,
+            block,
+            foil,
+            fine_wire,
+            no_abs_recipe
+        );
+        
+        event.create('hihiirokane')
+        //
+        .element(GTElements.get('hihiirokan'))
+        .color(0xF24F7D).secondaryColor(0xF24F00).iconSet(METALLIC)
+        .ingot()
+        .fluid()
+        .ore()
+        .cableProperties(V('uv'), 3, 0, true)
+        .blastTemp(3972, 'high', 58470, 510)
+        .flags(
+            plates,
+            dense_plate,
+            rod,
+            gear,
+            small_gear,
+            long_rod,
+            block,
+            foil,
+            fine_wire,
+            no_abs_recipe
+        );
+        
+        event.create('prometheum')
+        //
+        .element(GTElements.get('prometheum'))
+        .color(0x5A8156).secondaryColor(0x354D33).iconSet(DULL)
+        .ingot()
+        .fluid()
+        .ore()
+        .cableProperties(V('hv'), 3, 0, true)
+        .blastTemp(1580, null, 620, 185)
+        .flags(
+            plates,
+            dense_plate,
+            rod,
+            gear,
+            small_gear,
+            long_rod,
+            block,
+            foil,
+            fine_wire,
+            no_abs_recipe
+        );
+        
+        event.create('orichalcum')
+        //
+        .element(GTElements.get('Orichalcum'))
+        .color(0x780874).secondaryColor(0xF871E3).iconSet(METALLIC)
+        .ingot()
+        .fluid()
+        .ore()
+        .cableProperties(V('zpm'), 3, 0, false)
+        .blastTemp(2313, 'high', 2100, 720)
+        .flags(
+            plates,
+            dense_plate,
+            rod,
+            gear,
+            small_gear,
+            long_rod,
+            block,
+            foil,
+            fine_wire,
+            no_abs_recipe
+        );
 
-        event.create('mana')
-            .element(GTElements.get('mana'))
-            .fluid()
-            .color(0x20DAFF)
-            .fluidTemp(271)
-            .composition()
+        event.create('mana_steel')
+        //Fe₃✬₃
+        .components('3x iron', '3x mana')
+        .color(0x67b9ee).secondaryColor(0xB5FFFC).iconSet(METALLIC)
+        .ingot()
+        .fluid()
+        .cableProperties(V('mv'), 2, 0, false)
+        .flags(
+            foil,
+            fine_wire,
+            plates,
+            rod,
+            frame,
+            gear,
+            long_rod
+        );
+        
+        event.create('elementium')
+        //(Fe₃✬₃)₂(SiO₂)₄Fe
+//        .components('2x mana_steel', '3x amethyst', '2x silver')
+        .color(0xE084A5).secondaryColor(0xC543A8).iconSet(SHINY)
+        .fluid()
+        .ingot()
+        .cableProperties(V('ev'), 4, 0, false)
+        .flags(
+            foil,
+            fine_wire,
+            plates,
+            rod,
+            frame,
+            gear,
+            long_rod
+        );
+        
+        event.create('terra_steel')
+        //(Fe₃✬₃)₃✬₆V₃
+//        .components('3x mana_steel', '6x mana', '3x vanadium')
+        .color(0xCCFFB5).secondaryColor(0x2BB93B).iconSet(SHINY)
+        .fluid()
+        .ingot()
+        .cableProperties(V('iv'), 4, 0, true)
+        .flags(
+            foil,
+            fine_wire,
+            plates,
+            rod,
+            frame,
+            gear,
+            long_rod
+        );
+
+        
+        event.create('alfsteel')
+        //W₂Ma₇((Fe₃✬₃)₂(SiO₂)₄Fe)₂
+//        .components('2x tungsten', '7x mana', '2x elementium')
+        .color(0xFFC74E).secondaryColor(0xE27500).iconSet(SHINY)
+        .fluid()
+        .ingot()
+        .cableProperties(V('luv'), 6, 0, true)
+        .blastTemp(2759, 'mid', 5800, 100)
+        .flags(
+            foil,
+            fine_wire,
+            plates,
+            rod,
+            frame,
+            gear,
+            long_rod,
+            no_abs_recipe 
+        );
+        
     }
 )
+
+
+GTCEuStartupEvents.materialModification(
+    event => {
+//        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('mana_steel'),   () => Item.getItem('botania:manasteel_block'));
+//        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('mana_steel'),   () => Item.getItem('botania:manasteel_ingot'));
+//        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('mana_steel'),   () => Item.getItem('botania:manasteel_nugget'));
+//        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('terra_steel'),  () => Item.getItem('botania:terrasteel_block'));
+//        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('terra_steel'),  () => Item.getItem('botania:terrasteel_ingot'));
+//        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('terra_steel'),  () => Item.getItem('botania:terrasteel_nugget'));
+//        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcane_gold'),  () => Item.getItem('wizards_reborn:arcane_gold_block'));
+//        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcane_gold'),  () => Item.getItem('wizards_reborn:arcane_gold_ingot'));
+//        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcane_gold'),  () => Item.getItem('wizards_reborn:arcane_gold_nugget'));
+//        TagPrefix.block[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('alfsteel'),     () => Item.getItem('mythicbotany:alfsteel_block'));
+//        TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('alfsteel'),     () => Item.getItem('mythicbotany:alfsteel_ingot'));
+//        TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('alfsteel'),     () => Item.getItem('mythicbotany:alfsteel_nugget'));
+    }
+);
