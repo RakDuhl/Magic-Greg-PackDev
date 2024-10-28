801573c03215431668f6eaf7a49b42dd7bbd812c
RakDuhl <mikael@hake.ch>
2024-10-28 19:49:40 +0100

    Removed couple lines that cause crashing

--- kubejs/startup_scripts/GTAtomphysx/tagPrefix schiissdreck.js
+++ kubejs/startup_scripts/GTAtomphysx/tagPrefix schiissdreck.js
@@ -18,10 +18,10 @@ GTCEuStartupEvents.materialModification(
         TagPrefix.ingot[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:arcane_gold_ingot'));
         TagPrefix.nugget[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:arcane_gold_nugget'));
         TagPrefix.rawOre[   'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:raw_arcane_gold'));
-        TagPrefix.ore[      'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:arcane_gold_ore'));
-        TagPrefix.ore[      'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:deepslate_arcane_gold_ore'));
-        TagPrefix.ore[      'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:nether_arcane_gold_ore'));
-        TagPrefix.rawOreBlock['setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:raw_arcane_gold_block'));
+//        TagPrefix.ore[      'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:arcane_gold_ore'));
+//        TagPrefix.ore[      'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:deepslate_arcane_gold_ore'));
+//        TagPrefix.ore[      'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:nether_arcane_gold_ore'));
+//        TagPrefix.rawOreBlock['setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:raw_arcane_gold_block'));
         TagPrefix.plate[    'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:arcane_gold_sheet'));
         TagPrefix.crushed[  'setIgnored(com.gregtechceu.gtceu.api.data.chemical.material.Material,java.util.function.Supplier[])'](GTMaterials.get('arcanegold'),   () => Item.getItem('wizards_reborn:crushed_raw_arcane_gold'));
 
