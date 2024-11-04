1dad37aa34ee6fc35a23e311039b7500ff7467af
RakDuhl <mikael@hake.ch>
2024-11-04 22:45:27 +0100

    added vein.layer variables for ease of writing

--- kubejs/server_scripts/_init.js
+++ kubejs/server_scripts/_init.js
@@ -1,3 +1,11 @@
 //priority: 10000
 
+//variables
+
+    //OreGen variables
+    const stone = 'stone';
+    const deepslate = 'deepslate';
+    const netherrack = 'netherrack';
+    const endstone = 'endstone';
+
 //Functions I use elsewhere
