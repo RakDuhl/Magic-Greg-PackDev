0ca03fa2754078007f31ed2c0d654c66f85bd845
RakDuhl <mikael@hake.ch>
2024-11-04 22:43:03 +0100

    made a new script that contains normal composite materials

---
+++ kubejs/startup_scripts/GTAtomphysx/normal Materials.js
@@ -0,0 +1,38 @@
+//priority: 9310
+
+
+GTCEuStartupEvents.registry(
+    'gtceu:material',
+    material => {
+        material.create('dilithiumalumite')
+        .components('2x lithium', 'aluminium')
+        .dust()
+        .color(0x70A593).iconSet(FINE)
+        .flags(centrifuge);
+
+        material.create('borate3')
+        .components('3x boron', '9x oxygen')
+        .dust()
+        .color(0x6EA34B).iconSet(FINE)
+        .flags(electrolyze);
+
+        material.create('hypersilicate')
+        .components('6x silicon', '18x oxygen')
+        .dust()
+        .color(0x4F935F).iconSet(FINE)
+        .flags(electrolyze);
+
+        material.create('hydroxyl')
+        .components('3x oxygen', '3x hydrogen')
+        .liquid()
+        .color(0x471E22)
+        .flags(electrolyze);
+    }
+)
+
+GTCEuStartupEvents.materialModification(
+    event => {
+        GTMaterials.get('borate3').setFormula('(BO3)3')
+        GTMaterials.get('hydroxyl').setFormula('(OH)3')
+    }
+)
