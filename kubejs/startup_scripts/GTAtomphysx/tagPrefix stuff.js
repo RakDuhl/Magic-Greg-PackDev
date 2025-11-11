cb543c2cf38dbbc0b98edff9bb8f419fd9a4aeaf
Ra'Ahm <mikael@hake.ch>
2025-11-11 21:20:59 +0100

    TagPrefix head scratcher

--- kubejs/startup_scripts/GTAtomphysx/tagPrefix stuff.js
+++ kubejs/startup_scripts/GTAtomphysx/tagPrefix stuff.js
@@ -43,10 +43,49 @@ GTCEuStartupEvents.registry(
     }
 )
 
-
+//console.log("before:" + ChemicalHelper.get(TagPrefix.ore, GTMaterials.get('arcanum'), 1))   //Was a test to find out what happens, idk either, jurrejelle gave me that
 
 GTCEuStartupEvents.materialModification(
     event => {
+        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('manasteel'), () => Item.getItem('botania:manasteel_block'));
+        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('manasteel'), () => Item.getItem('botania:manasteel_ingot'));
+        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('manasteel'), () => Item.getItem('botania:manasteel_nugget'));
+        
+        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('terrasteel'), () => Item.getItem('botania:terrasteel_block'));
+        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('terrasteel'), () => Item.getItem('botania:terrasteel_ingot'));
+        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('terrasteel'), () => Item.getItem('botania:terrasteel_nugget'));
+
+        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('elementium'), () => Item.getItem('botania:elementium_block'));
+        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('elementium'), () => Item.getItem('botania:elementium_ingot'));
+        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('elementium'), () => Item.getItem('botania:terrasteel_nugget'));
+
+        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_block'));
+        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_ingot'));
+        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_nugget'));
+        TagPrefix.plate[TagPrefixRule]          (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_sheet'));
+        //TagPrefix.ore[TagPrefixRule]            (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_ore'));
+        //TagPrefix.oreDeepslate[TagPrefixRule]   (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:deepslate_arcane_gold_ore'));
+        TagPrefix.rawOre[TagPrefixRule]         (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:raw_arcane_gold'));
+        TagPrefix.rawOreBlock[TagPrefixRule]    (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:raw_arcane_gold_block'));
+
+        TagPrefix.rawOre[TagPrefixRule]         (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum'));
+        TagPrefix.dust[TagPrefixRule]           (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum_dust'));
+        TagPrefix.rawOreBlock[TagPrefixRule]    (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum_block'));
+        //TagPrefix.ore[TagPrefixRule]            (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum_ore'));
+        //TagPrefix.oreDeepslate[TagPrefixRule]   (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:deepslate_arcanum_ore'));
+
+        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('alfsteel'), () => Item.getItem('mythicbotany:alfsteel_block'));
+        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('alfsteel'), () => Item.getItem('mythicbotany:alfsteel_ingot'));
+        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('alfsteel'), () => Item.getItem('mythicbotany:alfsteel_nugget'));
+
+        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('ember'), () => Item.getItem('embers:ember_crystal_cluster'));
+        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('ember'), () => Item.getItem('embers:ember_crystal'));
+        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('ember'), () => Item.getItem('embers:ember_shard'));
+        TagPrefix.dust[TagPrefixRule]           (GTMaterials.get('ember'), () => Item.getItem('embers:ember_grit'));
+
+        TagPrefix.gem[TagPrefixRule]            (GTMaterials.get('rosequartz'),() => Item.getItem('create:rose_quartz'));
+        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('rosequartz'),() => Item.getItem('create:rose_quartz_block'));
+
 //        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('bluexychorium'), () => Item.getItem('xycraft_world:xychorium_storage_blue'));
 //        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('greenxychorium'), () => Item.getItem('xycraft_world:xychorium_storage_green'));
 //        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('redxychorium'), () => Item.getItem('xycraft_world:xychorium_storage_red'));
@@ -69,44 +108,7 @@ GTCEuStartupEvents.materialModification(
 //        TagPrefix.gem[TagPrefixRule]            (GTMaterials.get('redxychorium'), () => Item.getItem('xycraft_world:xychorium_gem_red'));
 //        TagPrefix.gem[TagPrefixRule]            (GTMaterials.get('darkxychorium'), () => Item.getItem('xycraft_world:xychorium_gem_dark'));
 //        TagPrefix.gem[TagPrefixRule]            (GTMaterials.get('lightxychorium'), () => Item.getItem('xycraft_world:xychorium_gem_light'));
-
-        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('manasteel'), () => Item.getItem('botania:manasteel_block'));
-        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('manasteel'), () => Item.getItem('botania:manasteel_ingot'));
-        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('manasteel'), () => Item.getItem('botania:manasteel_nugget'));
-        
-        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('terrasteel'), () => Item.getItem('botania:terrasteel_block'));
-        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('terrasteel'), () => Item.getItem('botania:terrasteel_ingot'));
-        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('terrasteel'), () => Item.getItem('botania:terrasteel_nugget'));
-
-        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('elementium'), () => Item.getItem('botania:elementium_block'));
-        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('elementium'), () => Item.getItem('botania:elementium_ingot'));
-        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('elementium'), () => Item.getItem('botania:terrasteel_nugget'));
-
-        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_block'));
-        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_ingot'));
-        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_nugget'));
-//        TagPrefix.rawOre[TagPrefixRule]         (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:raw_arcane_gold'));
-//        TagPrefix.ore[TagPrefixRule]            (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_ore'));
-//        TagPrefix.ore[TagPrefixRule]            (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:deepslate_arcane_gold_ore'));
-//        TagPrefix.ore[TagPrefixRule]            (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:nether_arcane_gold_ore'));
-//        TagPrefix.rawOreBlock[TagPrefixRule]    (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:raw_arcane_gold_block'));
-        TagPrefix.plate[TagPrefixRule]          (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:arcane_gold_sheet'));
-//        TagPrefix.crushed[TagPrefixRule]        (GTMaterials.get('arcanegold'), () => Item.getItem('wizards_reborn:crushed_raw_arcane_gold'));
-
-        //TagPrefix.ore[TagPrefixRule]            (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum_ore'));
-        //TagPrefix.oreDeepslate[TagPrefixRule]   (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:deepslate_arcanum_ore'));
-        TagPrefix.gem[TagPrefixRule]            (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum'));
-        TagPrefix.dust[TagPrefixRule]           (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum_dust'));
-        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('arcanum'), () => Item.getItem('wizards_reborn:arcanum_block'));
-
-        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('alfsteel'), () => Item.getItem('mythicbotany:alfsteel_block'));
-        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('alfsteel'), () => Item.getItem('mythicbotany:alfsteel_ingot'));
-        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('alfsteel'), () => Item.getItem('mythicbotany:alfsteel_nugget'));
-
-        TagPrefix.block[TagPrefixRule]          (GTMaterials.get('ember'), () => Item.getItem('embers:ember_crystal_cluster'));
-        TagPrefix.ingot[TagPrefixRule]          (GTMaterials.get('ember'), () => Item.getItem('embers:ember_crystal'));
-        TagPrefix.nugget[TagPrefixRule]         (GTMaterials.get('ember'), () => Item.getItem('embers:ember_shard'));
-        TagPrefix.dust[TagPrefixRule]           (GTMaterials.get('ember'), () => Item.getItem('embers:ember_grit'));
-
     }
 )
+
+console.log("after:" + ChemicalHelper.get(TagPrefix.ore, GTMaterials.get('arcanum'), 1))
